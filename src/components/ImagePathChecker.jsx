21702446ffe830590fd359ae30aabc97579ff3ff
Stephen Doty <stephendoty@Stephens-MacBook-Pro.local>
2024-07-01 23:18:09 -0500

    added more images

--- src/components/ImagePathChecker.jsx
+++ src/components/ImagePathChecker.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import helldivers2Data from "../gameData/helldivers2.json";
 
-console.log(helldivers2Data)
+console.log(helldivers2Data);
 
 const ImagePathChecker = () => {
   return (
     <div>
-      {helldivers2Data.helmets.map((el) => {
+      {helldivers2Data.stratagems.orbital.map((el) => {
+        console.log(el);
         return <img src={el.image} alt="" />;
       })}
     </div>
