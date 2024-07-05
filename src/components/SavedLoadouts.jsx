3a6fe253392c0c5f7b741b59bd8f94abbddacc1f
Stephen Doty <stephendoty@Stephens-MacBook-Pro.local>
2024-07-05 16:45:49 -0500

    Worked a bit on image sizing for modals. Got the reset and save buttons working. Did a bit of work on the savedLoadouts page

--- src/components/SavedLoadouts.jsx
+++ src/components/SavedLoadouts.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const SavedLoadouts = () => {
-  return <div>SavedLoadouts</div>;
+
+  let savedLoadoutsJSON = localStorage.getItem("savedLoadouts")
+
+  let savedLoadouts = JSON.parse(savedLoadoutsJSON)
+
+  return <div>
+    {savedLoadouts.map(savedLoadout => {
+      console.log('savedLoadout', savedLoadout);
+      return (
+        <div>
+          <ul>
+            {savedLoadout.stratagems.map((stratagem) => {
+              console.log("stratagem", stratagem);
+              return <li>{stratagem.name}</li>;
+            })}
+          </ul>
+          <ul>
+            {savedLoadout.armorSet.map((armorPiece) => {
+              console.log("armorPiece", armorPiece);
+              return <li>{armorPiece.name}</li>;
+            })}
+          </ul>
+          <ul>
+            {savedLoadout.equipment.map((equipment) => {
+              console.log("equipment", equipment);
+              return <li>{equipment.name}</li>;
+            })}
+          </ul>
+          <br />
+        </div>
+      );
+    })}
+  </div>;
 };
 
 export default SavedLoadouts;
